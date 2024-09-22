b5f9fb9ef047ad0c2aabac1489088da32d4ac00e
Avi-Pate <avi.patel@sprinklr.com>
2024-09-22 21:56:52 +0530

    chore: add home tab

--- src/components/header/constants.ts
+++ src/components/header/constants.ts
@@ -1,10 +1,12 @@
 export const TABS = {
+  HOME: "",
   PRODUCTS: "products",
   CUSTOMERS: "customers",
   ABOUT_US: "about_us",
 };
 
 export const NAV_ITEMS = [
+  { id: TABS.HOME, label: "Home" },
   { id: TABS.PRODUCTS, label: "Products" },
   { id: TABS.CUSTOMERS, label: "Customers" },
   { id: TABS.ABOUT_US, label: "About Us" },
